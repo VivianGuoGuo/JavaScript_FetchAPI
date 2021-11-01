60f3ed36a529f67cd85102fc67f33e15f22b9dab
VivianGuoGuo <47832438+VivianGuoGuo@users.noreply.github.com>
2021-11-01 14:22:29 -0700

    Add files via upload

---
+++ final_test/index.js
@@ -0,0 +1,45 @@
+let ARTIST_NAME='ad';
+const items=document.querySelector('.items');
+const search=document.querySelector('.fas');
+const input=document.querySelector('.input');
+let template = '';
+let newArist_name=''
+
+
+
+const url=`https://itunes.apple.com/search?term=${ARTIST_NAME}&media=music&entity=album&attribute=artistTerm&limit=200`;
+
+fetchJsonp(url)
+.then((res)=>res.json())
+.then((data)=>{
+displayResult(data.results);
+})
+
+input.addEventListener('change',(e)=>{
+    e.preventDefault();
+    let input=e.target.value;
+    search.addEventListener('click',()=>{
+        ARTIST_NAME=input;
+       newArist_name=ARTIST_NAME;
+       console.log(ARTIST_NAME);
+    })
+    console.log(ARTIST_NAME);
+
+})
+
+function displayResult(albumArr){
+    let template = '';
+    albumArr.forEach(album => {
+       // console.log(album.artistId);
+        template += 
+        `<div class="card" style="width: 18rem;">
+        <img src="${album.artworkUrl100}" class="card-img-top" alt="..."> 
+        <div class="card-body">
+          <p class="card-title">${album.collectionName}</p>
+        </div>
+      </div>`;
+    })
+     items.innerHTML=template;
+}
+
+
